37937ac9f6986469dd70dfbfaf59ab1f85bef266
shaper-pmp <shaper.pmp@gmail.com>
2013-11-30 18:14:36 +0000

    Refactored renderer init

--- renderer.js
+++ renderer.js
@@ -1,48 +1,27 @@
 var CanvasRenderer = function(engine, container) {
   
-  this.container = container && container.nodeType ? container : document.getElementById(container);  // If element given use that, otherwise assume it's the ID of an element to load
-  
-  if(this.container && this.container.tagName && this.container.tagName.toLowerCase() !== "canvas") { // If it's an ID but not of  canvas element, deleted it and replace the element with a canvas element with that ID
-    this.container.parentElement.removeChild(this.container);
-    this.container = null;
-  }
-  if(!this.container) {
-    var cv = document.createElement("canvas");
-    cv.id = container;
-    cv.width = engine.width * 10;
-    cv.height = engine.height * 10;
-    this.container = document.body.appendChild(cv);
-  }
-  this.ctx = this.container.getContext('2d');
-  
-  //console.log("Container (px)", this.container.width, this.container.height);
-  
   this.engine = engine;
   
-  //console.log("World (cells)", engine.width, engine.height);
-  
-  this.particleWidth = this.container.width / this.engine.width;
-  this.particleHeight = this.container.height / this.engine.height;
-  
-  //console.log("Particles", this.particleWidth, this.particleHeight);
-  
-  this.render = function() {
-    this.ctx.clearRect(0, 0, this.container.width, this.container.height);
+  this.init = function(container) {
+    this.container = container && container.nodeType ? container : document.getElementById(container);  // If element given use that, otherwise assume it's the ID of an element to load
     
-    for(var i=0; i<this.engine.width; i++) {
-      for(var j=0; j<this.engine.height; j++) {
-        var p = this.engine.getCell(i, j);
-        if(p) {
-          this.renderParticle(p);
-        }
-      }
+    if(this.container && this.container.tagName && this.container.tagName.toLowerCase() !== "canvas") { // If it's an ID but not of  canvas element, deleted it and replace the element with a canvas element with that ID
+      this.container.parentElement.removeChild(this.container);
+      this.container = null;
     }
-  };
-  
-  this.renderParticle = function(p) {
-    //console.log("Drawing ", p.material.name, "particle at ", p.x*this.particleWidth, p.y*this.particleHeight);
-    this.ctx.fillStyle = p.material.colour;
-    this.ctx.fillRect(p.x*this.particleWidth, p.y*this.particleHeight, this.particleWidth, this.particleHeight);
+    if(!this.container) {
+      var cv = document.createElement("canvas");
+      cv.id = container;
+      cv.width = engine.width * 10;
+      cv.height = engine.height * 10;
+      this.container = document.body.appendChild(cv);
+    }
+    this.ctx = this.container.getContext('2d');
+    
+    this.particleWidth = this.container.width / this.engine.width;
+    this.particleHeight = this.container.height / this.engine.height;
+    
+    this.setUpEvents();
   };
   
   this.setUpEvents = function() {
@@ -66,8 +45,27 @@ var CanvasRenderer = function(engine, container) {
       };
     })(this, this.engine)
     );
+  };
+
   
+  this.render = function() {
+    this.ctx.clearRect(0, 0, this.container.width, this.container.height);
+    
+    for(var i=0; i<this.engine.width; i++) {
+      for(var j=0; j<this.engine.height; j++) {
+        var p = this.engine.getCell(i, j);
+        if(p) {
+          this.renderParticle(p);
+        }
+      }
+    }
   };
   
-  this.setUpEvents();
+  this.renderParticle = function(p) {
+    //console.log("Drawing ", p.material.name, "particle at ", p.x*this.particleWidth, p.y*this.particleHeight);
+    this.ctx.fillStyle = p.material.colour;
+    this.ctx.fillRect(p.x*this.particleWidth, p.y*this.particleHeight, this.particleWidth, this.particleHeight);
+  };
+  
+  this.init(container);
 };
